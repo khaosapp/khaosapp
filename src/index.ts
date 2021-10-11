64484001af3c89ea797be49f2fa0ad4be7208c03
Bruno <bruno.martins@bewgpt.com.pt>
2021-10-11 10:40:59 +0100

    Test basic electron funcionality

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
 
+function rotateText(document: any): void {
+  console.log(document)
+}
+
+// Quit when all windows are closed. 
+app.on('window-all-closed', function() 
+{ 
+  // On OS X it is common for applications and their 
+  // menu barto stay active until the user quits  
+  // explicitly with Cmd + Q if (process.platform != 'darwin') 
+  { 
+    app.quit(); 
+  } 
+}); 
+
 app.on("ready", () => {
   console.log("App is ready");
   const win = new BrowserWindow({
@@ -8,7 +23,7 @@ app.on("ready", () => {
     height: 400,
   });
   const indexHTML = path.join(__dirname + "/index.html");
-  win.loadFile(indexHTML).then(() => {
-    // IMPLEMENT FANCY STUFF HERE
+  win.loadFile(indexHTML).then((document) => {
+      console.log(document);
   });
 });
